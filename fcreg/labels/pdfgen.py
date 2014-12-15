a8b91715638dcd60abf4de78bb0edc643aa19e71
Dax <dax@furtherconfusion.org>
2014-12-14 20:55:57 -0800

    Read and sort CSV.

--- fcreg/labels/pdfgen.py
+++ fcreg/labels/pdfgen.py
@@ -2,7 +2,10 @@ import csv
 import os
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
 import sys
+
+FONT_NAME = "Open Sans"
 
 def main(argv):
   if len(argv) < 3:
@@ -10,7 +13,18 @@ def main(argv):
     sys.exit(2)
   _, csv_file, pdf_file = argv[:3]
 
+  data = None
+  with open(csv_file, "r") as csv_file_object:
+    data = list(csv.DictReader(csv_file_object))
+    data.sort(key=lambda x: x["fanname"])
+    data.sort(key=lambda x: x["firstname"])
+    data.sort(key=lambda x: x["lastname"])
+
+  for row in data:
+    print "Processing %s %s (%s)" % (row["firstname"], row["lastname"], row["fanname"])
+
   c = canvas.Canvas(pdf_file, pagesize=letter)
+  c.drawString(100, 100, "Hello World")
   c.showPage()
   c.save()
 

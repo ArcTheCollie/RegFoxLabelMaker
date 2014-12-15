67a844e37a874ca35758fe68b2523e8a7c090736
Dax <dax@furtherconfusion.org>
2014-12-14 18:35:14 -0800

    Accept and run tests.

--- runtest.sh
+++ runtest.sh
@@ -3,5 +3,5 @@
 set -eu
 
 [[ -f test.pdf ]] && rm test.pdf
-python fcreg/labels/pdfgen.py test.pdf
+python fcreg/labels/pdfgen.py test.csv test.pdf
 [[ -f test.pdf ]] || { echo "test.pdf does not exist" >&2; exit 2; }

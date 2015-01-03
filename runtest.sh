1505e1f062dd2eea6fe8bbc1d20a21acbe3bf408
Dax <dax@furtherconfusion.org>
2015-01-02 22:54:23 -0800

    Call by module name, not by filename.

--- runtest.sh
+++ runtest.sh
@@ -3,11 +3,11 @@
 set -eu
 
 [[ -f "test.pdf" ]] && rm test.pdf
-python fcreg/labels/pdfgen.py -v test.csv test.pdf
+python -m fcreg.labels.pdfgen -v test.csv test.pdf
 [[ -f test.pdf ]] || { echo "Fail: test.pdf does not exist." >&2; exit 2; }
 
 [[ -f "test_with_outlines.pdf" ]] && rm test_with_outlines.pdf
-python fcreg/labels/pdfgen.py --outlines -v test.csv test_with_outlines.pdf
+python -m fcreg.labels.pdfgen --outlines -v test.csv test_with_outlines.pdf
 [[ -f test_with_outlines.pdf ]] \
     || { echo "Fail: test_with_outlines.pdf does not exist." >&2; exit 2; }
 echo "Pass."
